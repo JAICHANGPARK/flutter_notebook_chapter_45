2c9514f31b264b8bbbaf8320c429b7afb2b284d5
JAICHANGPARK <aristojeff@gmail.com>
2025-05-19 18:27:12 +0900

    Update ep2251_main.dart

--- lib/ep2251_pos_mobile_app/ep2251_main.dart
+++ lib/ep2251_pos_mobile_app/ep2251_main.dart
@@ -5,3 +5,11 @@ void main() {
 
 }
 
+class PosMobileApp extends StatelessWidget {
+  const PosMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
