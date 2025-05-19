3bd015dc0b4f07fa67921ec6b08d2ffecd9110d2
JAICHANGPARK <aristojeff@gmail.com>
2025-05-19 18:28:47 +0900

    Update pos_home_screen.dart

---
+++ lib/ep2251_pos_mobile_app/home/pos_home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class PosHomeScreen extends StatefulWidget {
+  const PosHomeScreen({super.key});
+
+  @override
+  State<PosHomeScreen> createState() => _PosHomeScreenState();
+}
+
+class _PosHomeScreenState extends State<PosHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
