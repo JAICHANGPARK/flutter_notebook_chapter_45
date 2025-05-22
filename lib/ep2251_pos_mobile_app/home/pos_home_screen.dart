8b714e2ca572802e3770fc333a39a9dcd9dbdf5a
JAICHANGPARK <aristojeff@gmail.com>
2025-05-22 18:21:02 +0900

    Update pos_home_screen.dart

--- lib/ep2251_pos_mobile_app/home/pos_home_screen.dart
+++ lib/ep2251_pos_mobile_app/home/pos_home_screen.dart
@@ -148,6 +148,7 @@ class _PosHomeScreenState extends State<PosHomeScreen> {
                                         decoration: BoxDecoration(
                                           shape: BoxShape.circle
                                         ),
+                                        child: Text("🍔"),
                                       )
                                     ],
                                   )
