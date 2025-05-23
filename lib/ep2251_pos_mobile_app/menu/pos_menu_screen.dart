594be0c61fa0f8bde695a591b9fb2e7f455f9676
JAICHANGPARK <aristojeff@gmail.com>
2025-05-24 08:15:07 +0900

    Update pos_menu_screen.dart

--- lib/ep2251_pos_mobile_app/menu/pos_menu_screen.dart
+++ lib/ep2251_pos_mobile_app/menu/pos_menu_screen.dart
@@ -93,7 +93,13 @@ class _PosMenuScreenState extends State<PosMenuScreen> {
               ),
               Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
               maxLines: 2,),
-              Text("subtitle"),
+              Row(
+                  spacing: 8,
+                  children: [
+                Text("\$9.87"),
+
+
+              ]),
               Text("Spice Level"),
               SizedBox(height: 72, child: Placeholder()),
               Text("Extras"),
