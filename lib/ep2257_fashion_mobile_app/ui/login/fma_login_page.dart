b46512dd96bf9ead6d90ac9d4282c3a524553941
JAICHANGPARK <aristojeff@gmail.com>
2025-05-25 08:23:54 +0900

    Update fma_login_page.dart

---
+++ lib/ep2257_fashion_mobile_app/ui/login/fma_login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class FmaLoginPage extends StatefulWidget {
+  const FmaLoginPage({super.key});
+
+  @override
+  State<FmaLoginPage> createState() => _FmaLoginPageState();
+}
+
+class _FmaLoginPageState extends State<FmaLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
