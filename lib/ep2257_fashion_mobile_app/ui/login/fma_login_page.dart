b9fb801b99a97377bf68616db7a1dbb6943e2cc6
JAICHANGPARK <aristojeff@gmail.com>
2025-05-25 08:27:54 +0900

    Update fma_login_page.dart

--- lib/ep2257_fashion_mobile_app/ui/login/fma_login_page.dart
+++ lib/ep2257_fashion_mobile_app/ui/login/fma_login_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class FmaLoginPage extends StatefulWidget {
   const FmaLoginPage({super.key});
 
@@ -15,7 +13,10 @@ class _FmaLoginPageState extends State<FmaLoginPage> {
     return Scaffold(
       body: Column(
         children: [
-          Text("Elevate Your Fashion Style Made Simple")
+          Text("Elevate Your Fashion Style Made Simple"),
+          Text(
+            'Explore curated trends, personalize your shopping experience and elevate your wardrobe - all in one place. Fashion made just for you!'
+          ),
 
         ],
       ),
