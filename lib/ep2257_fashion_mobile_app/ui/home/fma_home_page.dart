95eb59aa469e3c46027e017b9ed1eb373084b14e
JAICHANGPARK <aristojeff@gmail.com>
2025-05-26 19:52:57 +0900

    Update fma_home_page.dart

--- lib/ep2257_fashion_mobile_app/ui/home/fma_home_page.dart
+++ lib/ep2257_fashion_mobile_app/ui/home/fma_home_page.dart
@@ -11,6 +11,12 @@ class FmaHomePage extends StatefulWidget {
 class _FmaHomePageState extends State<FmaHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
