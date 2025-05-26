913775772bbe74050b72902743726b346de1d128
JAICHANGPARK <aristojeff@gmail.com>
2025-05-26 19:52:46 +0900

    Update fma_home_page.dart

---
+++ lib/ep2257_fashion_mobile_app/ui/home/fma_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FmaHomePage extends StatefulWidget {
+  const FmaHomePage({super.key});
+
+  @override
+  State<FmaHomePage> createState() => _FmaHomePageState();
+}
+
+class _FmaHomePageState extends State<FmaHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
