51d384956891424c36856cc5dc3f2aa48e930bc0
JAICHANGPARK <aristojeff@gmail.com>
2025-05-29 22:56:25 +0900

    Update ep2261_main.dart

--- lib/ep2261_stay_booking_app/ep2261_main.dart
+++ lib/ep2261_stay_booking_app/ep2261_main.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  
+}
+
+class StayBookingApp extends StatelessWidget {
+  const StayBookingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
