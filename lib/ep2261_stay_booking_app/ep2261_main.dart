28e34ddf55b45d37f8becc0b478e69f215b3e2a3
JAICHANGPARK <aristojeff@gmail.com>
2025-05-29 22:56:18 +0900

    Update ep2261_main.dart

--- lib/ep2261_stay_booking_app/ep2261_main.dart
+++ lib/ep2261_stay_booking_app/ep2261_main.dart
@@ -1,16 +1,2 @@
 import 'package:flutter/material.dart';
 
-
-class StayBookingApp extends StatefulWidget {
-  const StayBookingApp({super.key});
-
-  @override
-  State<StayBookingApp> createState() => _StayBookingAppState();
-}
-
-class _StayBookingAppState extends State<StayBookingApp> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
