541be9492f94b0eb7a81ff214a5c89276a3526ef
JAICHANGPARK <aristojeff@gmail.com>
2025-05-29 22:58:37 +0900

    Create stay_booking_home_screen.dart

---
+++ lib/ep2261_stay_booking_app/ui/home/stay_booking_home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class StayBookingHomeScreen extends StatefulWidget {
+  const StayBookingHomeScreen({super.key});
+
+  @override
+  State<StayBookingHomeScreen> createState() => _StayBookingHomeScreenState();
+}
+
+class _StayBookingHomeScreenState extends State<StayBookingHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
