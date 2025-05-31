0d46999cee118c31a09c9b624dbfd8411603c84c
JAICHANGPARK <aristojeff@gmail.com>
2025-06-01 08:23:12 +0900

    Update stay_detail_page.dart

--- lib/ep2261_stay_booking_app/ui/detail/stay_detail_page.dart
+++ lib/ep2261_stay_booking_app/ui/detail/stay_detail_page.dart
@@ -54,23 +54,11 @@ class _StayDetailPageState extends State<StayDetailPage> {
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
               maxLines: 3,
             ),
-            Row(
-              children: [
-                Text("Read More"),
-                Icon(Icons.arrow_drop_down)
-              ],
-            ),
-            Container(
-              height: 140,
-              child: Placeholder(),
-            ),
+            Row(children: [Text("Read More"), Icon(Icons.arrow_drop_down)]),
+            Container(height: 140, child: Placeholder()),
             Divider(),
             Expanded(child: Placeholder()),
-            Container(
-              height: 80,
-              child: Placeholder(),
-            )
-
+            Container(height: 80, child: Placeholder()),
           ],
         ),
       ),
