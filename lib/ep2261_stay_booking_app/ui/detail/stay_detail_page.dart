5455849a7db455cfc0aa1d0f95d02b5e21c2891c
JAICHANGPARK <aristojeff@gmail.com>
2025-05-31 10:15:49 +0900

    Update stay_detail_page.dart

--- lib/ep2261_stay_booking_app/ui/detail/stay_detail_page.dart
+++ lib/ep2261_stay_booking_app/ui/detail/stay_detail_page.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
 
+class StayDetailPage extends StatefulWidget {
+  const StayDetailPage({super.key});
+
+  @override
+  State<StayDetailPage> createState() => _StayDetailPageState();
+}
+
+class _StayDetailPageState extends State<StayDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
