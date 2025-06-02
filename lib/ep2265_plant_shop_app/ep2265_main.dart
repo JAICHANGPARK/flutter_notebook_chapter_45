3b9196f9527adc36585b01e14d478a8bd7979fc0
JAICHANGPARK <aristojeff@gmail.com>
2025-06-02 19:07:12 +0900

    Update ep2265_main.dart

--- lib/ep2265_plant_shop_app/ep2265_main.dart
+++ lib/ep2265_plant_shop_app/ep2265_main.dart
@@ -3,3 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   
 }
+
+class PlantShopApp extends StatelessWidget {
+  const PlantShopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
