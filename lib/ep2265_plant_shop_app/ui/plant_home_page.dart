a14daf0d2985f7e4e72cbb8c1819ae75b3351463
JAICHANGPARK <aristojeff@gmail.com>
2025-06-02 19:07:55 +0900

    Update plant_home_page.dart

--- lib/ep2265_plant_shop_app/ui/plant_home_page.dart
+++ lib/ep2265_plant_shop_app/ui/plant_home_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class PlantHomePage extends StatefulWidget {
+  const PlantHomePage({super.key});
+
+  @override
+  State<PlantHomePage> createState() => _PlantHomePageState();
+}
+
+class _PlantHomePageState extends State<PlantHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
