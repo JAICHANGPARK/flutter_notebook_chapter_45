ed6062d2ee3db0480de3801c1248b3de44e2b898
JAICHANGPARK <aristojeff@gmail.com>
2025-06-03 09:06:30 +0900

    Update plant_home_page.dart

--- lib/ep2265_plant_shop_app/ui/plant_home_page.dart
+++ lib/ep2265_plant_shop_app/ui/plant_home_page.dart
@@ -188,15 +188,19 @@ class _PlantHomePageState extends State<PlantHomePage> {
                               ),
                               child: Row(
                                 children: [
-                                  Container(
-                                    width: 120,
-                                    child: Placeholder(),
+                                  Container(width: 120, child: Placeholder()),
+                                  Expanded(
+                                    child: Column(
+                                      children: [
+                                        Text("Dracaena Fragrans"),
+                                        Text(
+                                          "Dracaena fragrans (cornstalk dracaena), is a flowering plant species that is native to tropical Africa, from Sudan south to Mozambique, west to Côte d'Ivoire and southwest to Angola, growing in upland regions at 600–2,250 m (1,970–7,380 ft) altitude",
+                                          maxLines: 2,
+                                        ),
+                                        
+                                      ],
+                                    ),
                                   ),
-                                  Column(
-                                    children: [
-                                      
-                                    ],
-                                  )
                                 ],
                               ),
                             ),
