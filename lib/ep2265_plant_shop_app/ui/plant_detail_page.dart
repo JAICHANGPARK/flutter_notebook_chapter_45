59c03c53474c68f68c192f99d73d8dd3f0f63f2e
JAICHANGPARK <aristojeff@gmail.com>
2025-06-05 18:50:12 +0900

    Update plant_detail_page.dart

--- lib/ep2265_plant_shop_app/ui/plant_detail_page.dart
+++ lib/ep2265_plant_shop_app/ui/plant_detail_page.dart
@@ -106,7 +106,9 @@ class _PlantDetailPageState extends State<PlantDetailPage> {
                   ),
                   Row(
                     children: [
-                      Column(children: [Text("Monstera"), Text("\$240")]),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [Text("Monstera"), Text("\$240")]),
                       Row(
                         children: List.generate(
                           5,
@@ -116,6 +118,10 @@ class _PlantDetailPageState extends State<PlantDetailPage> {
                       ),
                     ],
                   ),
+                  Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,)
                 ],
               ),
             ),
