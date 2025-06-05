d0c4b2d0807514effb58537b986a0cf47ad583fa
JAICHANGPARK <aristojeff@gmail.com>
2025-06-05 18:50:35 +0900

    Update plant_detail_page.dart

--- lib/ep2265_plant_shop_app/ui/plant_detail_page.dart
+++ lib/ep2265_plant_shop_app/ui/plant_detail_page.dart
@@ -121,7 +121,12 @@ class _PlantDetailPageState extends State<PlantDetailPage> {
                   Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 
                   maxLines: 3,
-                  overflow: TextOverflow.ellipsis,)
+                  overflow: TextOverflow.ellipsis,),
+                  Text("Monstera Leaves"),
+                  SizedBox(
+                    height: 120,
+                    child: Placeholder(),
+                  )
                 ],
               ),
             ),
