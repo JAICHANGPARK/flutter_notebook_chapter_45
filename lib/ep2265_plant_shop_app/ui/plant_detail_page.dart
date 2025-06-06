1e237138a9ecb03d9e65954e5ba8824577a685a7
JAICHANGPARK <aristojeff@gmail.com>
2025-06-07 07:52:04 +0900

    Update plant_detail_page.dart

--- lib/ep2265_plant_shop_app/ui/plant_detail_page.dart
+++ lib/ep2265_plant_shop_app/ui/plant_detail_page.dart
@@ -5,6 +5,10 @@ class WaveClipper extends CustomClipper<Path>{
   @override
   Path getClip(Size size) {
     var path = Path();
+    path.lineTo(0, size.height * .8);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
     return path;
   }
 
