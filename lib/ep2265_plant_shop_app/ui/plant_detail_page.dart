b5436e9b13daea708eaa4232f01232e1638bd711
JAICHANGPARK <aristojeff@gmail.com>
2025-06-07 08:07:09 +0900

    Update plant_detail_page.dart

--- lib/ep2265_plant_shop_app/ui/plant_detail_page.dart
+++ lib/ep2265_plant_shop_app/ui/plant_detail_page.dart
@@ -79,7 +79,7 @@ class _PlantDetailPageState extends State<PlantDetailPage> {
             child: ClipPath(
               clipper: WaveClipper(),
               child: Container(
-                height: MediaQuery.sizeOf(context).height / 1.8,
+                height: MediaQuery.sizeOf(context).height / 1.3,
                 decoration: BoxDecoration(color: Colors.white),
               ),
             ),
