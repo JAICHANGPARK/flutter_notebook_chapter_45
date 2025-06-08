88217e183040e0fa0c020e9989360b1f95c24f64
JAICHANGPARK <aristojeff@gmail.com>
2025-06-08 10:39:42 +0900

    Create car_parking_home_page.dart

---
+++ lib/ep2271_parking_mobile_app/ui/car_parking_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class CarParkingHomePage extends StatefulWidget {
+  const CarParkingHomePage({super.key});
+
+  @override
+  State<CarParkingHomePage> createState() => _CarParkingHomePageState();
+}
+
+class _CarParkingHomePageState extends State<CarParkingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
