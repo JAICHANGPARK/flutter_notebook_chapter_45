e81528f7c09f0712a7a9dae7c668c820e1630b35
JAICHANGPARK <aristojeff@gmail.com>
2025-06-08 10:38:59 +0900

    Update ep2271_main.dart

---
+++ lib/ep2271_parking_mobile_app/ep2271_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class CarParkingMobileApp extends StatelessWidget {
+  const CarParkingMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
