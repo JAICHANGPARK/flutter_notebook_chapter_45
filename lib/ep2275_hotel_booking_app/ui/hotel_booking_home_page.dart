025ecadc80a1726c9c73b489119e2d2c1edabfef
JAICHANGPARK <aristojeff@gmail.com>
2025-06-13 19:19:09 +0900

    Update hotel_booking_home_page.dart

--- lib/ep2275_hotel_booking_app/ui/hotel_booking_home_page.dart
+++ lib/ep2275_hotel_booking_app/ui/hotel_booking_home_page.dart
@@ -11,7 +11,17 @@ class _HotelBookingHomePageState extends State<HotelBookingHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [Positioned.fill(child: Column(children: []))]),
+      body: Stack(
+        children: [
+          Positioned.fill(child: Column(children: [])),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              decoration: ShapeDecoration(shape: StadiumBorder()),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
