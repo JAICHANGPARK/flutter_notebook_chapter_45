ff858c0af957f47c05f9e965f58ea839ec7f5aac
JAICHANGPARK <aristojeff@gmail.com>
2025-06-14 08:45:09 +0900

    Update hotel_booking_home_page.dart

--- lib/ep2275_hotel_booking_app/ui/hotel_booking_home_page.dart
+++ lib/ep2275_hotel_booking_app/ui/hotel_booking_home_page.dart
@@ -14,6 +14,7 @@ class _HotelBookingHomePageState extends State<HotelBookingHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(250, 244, 240, 1),
       body: Stack(
         children: [
           Positioned.fill(
