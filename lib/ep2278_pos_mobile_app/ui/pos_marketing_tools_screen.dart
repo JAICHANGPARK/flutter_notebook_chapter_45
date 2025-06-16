54a936fa8b3c4ae072f51fa0e88c8674a93411b3
JAICHANGPARK <aristojeff@gmail.com>
2025-06-16 17:07:07 +0900

    Update pos_marketing_tools_screen.dart

--- lib/ep2278_pos_mobile_app/ui/pos_marketing_tools_screen.dart
+++ lib/ep2278_pos_mobile_app/ui/pos_marketing_tools_screen.dart
@@ -11,6 +11,16 @@ class PosMarketingToolsScreen extends StatefulWidget {
 class _PosMarketingToolsScreenState extends State<PosMarketingToolsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+        
+          ],
+        ),
+      ),
+    );
   }
 }
