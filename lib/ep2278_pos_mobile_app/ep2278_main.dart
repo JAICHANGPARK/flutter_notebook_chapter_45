a0ee570fca51323d3bcb8ad482d006f60b39bc2f
JAICHANGPARK <aristojeff@gmail.com>
2025-06-16 17:05:30 +0900

    Update ep2278_main.dart

---
+++ lib/ep2278_pos_mobile_app/ep2278_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+
+class PosMobileApp extends StatelessWidget {
+  const PosMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
