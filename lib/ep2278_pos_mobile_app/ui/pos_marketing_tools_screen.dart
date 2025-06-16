6abaad72a8afdd4f9ae58d5478e9ab05aa4ff1c0
JAICHANGPARK <aristojeff@gmail.com>
2025-06-16 17:05:08 +0900

    Update pos_marketing_tools_screen.dart

---
+++ lib/ep2278_pos_mobile_app/ui/pos_marketing_tools_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class PosMarketingToolsScreen extends StatefulWidget {
+  const PosMarketingToolsScreen({super.key});
+
+  @override
+  State<PosMarketingToolsScreen> createState() => _PosMarketingToolsScreenState();
+}
+
+class _PosMarketingToolsScreenState extends State<PosMarketingToolsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
