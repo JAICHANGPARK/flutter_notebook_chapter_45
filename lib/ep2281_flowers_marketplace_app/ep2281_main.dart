f9cd88b18a797585427f68fb72daa7c9d3d52e8e
JAICHANGPARK <aristojeff@gmail.com>
2025-06-20 18:45:47 +0900

    Update ep2281_main.dart

--- lib/ep2281_flowers_marketplace_app/ep2281_main.dart
+++ lib/ep2281_flowers_marketplace_app/ep2281_main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  
+}
+
 
 class FlowersMarketplaceApp extends StatelessWidget {
   const FlowersMarketplaceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
