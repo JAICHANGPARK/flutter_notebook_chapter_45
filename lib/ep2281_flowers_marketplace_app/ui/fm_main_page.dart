32692e29d5926b9b133f25dfcb2ae89840e62593
JAICHANGPARK <aristojeff@gmail.com>
2025-06-20 18:46:25 +0900

    Create fm_main_page.dart

---
+++ lib/ep2281_flowers_marketplace_app/ui/fm_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FmMainPage extends StatefulWidget {
+  const FmMainPage({super.key});
+
+  @override
+  State<FmMainPage> createState() => _FmMainPageState();
+}
+
+class _FmMainPageState extends State<FmMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
