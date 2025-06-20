d2ec55f8e26476a64c2c0b58e857a67a10a60eb1
JAICHANGPARK <aristojeff@gmail.com>
2025-06-20 18:45:37 +0900

    Create ep2281_main.dart

---
+++ lib/ep2281_flowers_marketplace_app/ep2281_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FlowersMarketplaceApp extends StatelessWidget {
+  const FlowersMarketplaceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
