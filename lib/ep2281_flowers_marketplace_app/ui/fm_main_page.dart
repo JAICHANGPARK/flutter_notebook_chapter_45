ab7d5e3e4911b320a588d802348f3c35f22a7321
JAICHANGPARK <aristojeff@gmail.com>
2025-06-20 18:46:43 +0900

    Update fm_main_page.dart

--- lib/ep2281_flowers_marketplace_app/ui/fm_main_page.dart
+++ lib/ep2281_flowers_marketplace_app/ui/fm_main_page.dart
@@ -11,6 +11,6 @@ class FmMainPage extends StatefulWidget {
 class _FmMainPageState extends State<FmMainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
