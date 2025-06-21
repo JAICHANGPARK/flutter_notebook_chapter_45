d7084220b54cd29de206df9011966e495f806782
JAICHANGPARK <aristojeff@gmail.com>
2025-06-22 08:01:53 +0900

    Update fm_detail_page.dart

--- lib/ep2281_flowers_marketplace_app/ui/fm_detail_page.dart
+++ lib/ep2281_flowers_marketplace_app/ui/fm_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FmDetailPage extends StatefulWidget {
   const FmDetailPage({super.key});
 
@@ -14,7 +13,20 @@ class _FmDetailPageState extends State<FmDetailPage> {
     return Scaffold(
       body: Stack(
         children: [
-
+          Container(
+            decoration: ShapeDecoration(
+              shape: RoundedSuperellipseBorder(
+                borderRadius: BorderRadius.circular(36),
+              ),
+              color: Colors.pink,
+              image: DecorationImage(
+                image: NetworkImage(
+                  "https://cdn.pixabay.com/photo/2016/11/23/15/42/bouquet-1853622_1280.jpg",
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
         ],
       ),
     );
