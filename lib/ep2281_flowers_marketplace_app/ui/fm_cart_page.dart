4e2dd5d590454c1c4fa44e9079393314b2109fc1
JAICHANGPARK <aristojeff@gmail.com>
2025-06-24 17:35:08 +0900

    Update fm_cart_page.dart

---
+++ lib/ep2281_flowers_marketplace_app/ui/fm_cart_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FmCartPage extends StatefulWidget {
+  const FmCartPage({super.key});
+
+  @override
+  State<FmCartPage> createState() => _FmCartPageState();
+}
+
+class _FmCartPageState extends State<FmCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
