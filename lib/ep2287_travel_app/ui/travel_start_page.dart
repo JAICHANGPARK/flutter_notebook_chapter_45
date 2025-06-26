2e0d922416d2241a4b6ed3d0a3204f504e389a4d
JAICHANGPARK <aristojeff@gmail.com>
2025-06-26 17:53:17 +0900

    Update travel_start_page.dart

--- lib/ep2287_travel_app/ui/travel_start_page.dart
+++ lib/ep2287_travel_app/ui/travel_start_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class TravelStartPage extends StatefulWidget {
+  const TravelStartPage({super.key});
+
+  @override
+  State<TravelStartPage> createState() => _TravelStartPageState();
+}
+
+class _TravelStartPageState extends State<TravelStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
