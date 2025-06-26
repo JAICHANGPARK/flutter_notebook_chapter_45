9b500a303097ba17a07d367aef5935f2a0c62334
JAICHANGPARK <aristojeff@gmail.com>
2025-06-26 17:53:32 +0900

    Update travel_start_page.dart

--- lib/ep2287_travel_app/ui/travel_start_page.dart
+++ lib/ep2287_travel_app/ui/travel_start_page.dart
@@ -10,6 +10,8 @@ class TravelStartPage extends StatefulWidget {
 class _TravelStartPageState extends State<TravelStartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(children: [],),
+    );
   }
 }
