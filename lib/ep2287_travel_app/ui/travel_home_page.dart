9b676501e12989872f441c4662c371b81421bfb0
JAICHANGPARK <aristojeff@gmail.com>
2025-06-27 18:42:59 +0900

    Update travel_home_page.dart

--- lib/ep2287_travel_app/ui/travel_home_page.dart
+++ lib/ep2287_travel_app/ui/travel_home_page.dart
@@ -21,6 +21,7 @@ class _TravelHomePageState extends State<TravelHomePage> {
             top: 32,
             child: SafeArea(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SizedBox(height: 62, child: Placeholder()),
                   Container(height: 120, child: Placeholder()),
