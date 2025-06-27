b55c0cce5097fa60accc2c788bc0471cacfb6ae4
JAICHANGPARK <aristojeff@gmail.com>
2025-06-27 18:42:53 +0900

    Update travel_home_page.dart

--- lib/ep2287_travel_app/ui/travel_home_page.dart
+++ lib/ep2287_travel_app/ui/travel_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TravelHomePage extends StatefulWidget {
   const TravelHomePage({super.key});
 
@@ -23,11 +22,14 @@ class _TravelHomePageState extends State<TravelHomePage> {
             child: SafeArea(
               child: Column(
                 children: [
-              
+                  SizedBox(height: 62, child: Placeholder()),
+                  Container(height: 120, child: Placeholder()),
+                  Text("YOUR TRAVELS"),
+                  SizedBox(height: 180, child: Placeholder()),
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
