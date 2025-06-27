27bda4c81eaf3ebdb284ab16742bda0d65ced806
JAICHANGPARK <aristojeff@gmail.com>
2025-06-27 18:48:26 +0900

    Update travel_home_page.dart

--- lib/ep2287_travel_app/ui/travel_home_page.dart
+++ lib/ep2287_travel_app/ui/travel_home_page.dart
@@ -42,14 +42,18 @@ class _TravelHomePageState extends State<TravelHomePage> {
             bottom: 32,
             left: 0,
             right: 0,
-            child: Container(
-              decoration: ShapeDecoration(
-                shape: StadiumBorder(),
-                color: Colors.grey[200]!,
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [CircleAvatar(), CircleAvatar(), CircleAvatar()],
+
+            child: Center(
+              child: Container(
+                decoration: ShapeDecoration(
+                  shape: StadiumBorder(),
+                  color: Colors.grey[200]!,
+                ),
+                child: Row(
+
+                  mainAxisSize: MainAxisSize.min,
+                  children: [CircleAvatar(), CircleAvatar(), CircleAvatar()],
+                ),
               ),
             ),
           ),
