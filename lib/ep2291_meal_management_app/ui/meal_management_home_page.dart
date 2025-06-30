5b9c80aeff4e70b4cdb47c92c0987424b08dda85
JAICHANGPARK <aristojeff@gmail.com>
2025-06-30 17:08:25 +0900

    Update meal_management_home_page.dart

--- lib/ep2291_meal_management_app/ui/meal_management_home_page.dart
+++ lib/ep2291_meal_management_app/ui/meal_management_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MealManagementHomePage extends StatefulWidget {
   const MealManagementHomePage({super.key});
 
@@ -12,7 +11,19 @@ class _MealManagementHomePageState extends State<MealManagementHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 20,
+      ),
+      floatingActionButton: FloatingActionButton(
 
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(32),
+        ),
+        onPressed: () {},
+        child: Icon(Icons.list_alt),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
