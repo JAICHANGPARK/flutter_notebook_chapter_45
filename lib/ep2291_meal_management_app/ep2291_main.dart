98595cf9ab2fa9be2d666d26c821081998509875
JAICHANGPARK <aristojeff@gmail.com>
2025-06-30 17:01:01 +0900

    Update ep2291_main.dart

---
+++ lib/ep2291_meal_management_app/ep2291_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  
+}
+
+class MealManagementApp extends StatelessWidget {
+  const MealManagementApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
