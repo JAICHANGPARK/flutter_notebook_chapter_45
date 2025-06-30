2e255c2cb8bc189a08f955b10970ceaf3a8031b3
JAICHANGPARK <aristojeff@gmail.com>
2025-06-30 17:03:25 +0900

    Create meal_management_home_page.dart

---
+++ lib/ep2291_meal_management_app/ui/meal_management_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MealManagementHomePage extends StatefulWidget {
+  const MealManagementHomePage({super.key});
+
+  @override
+  State<MealManagementHomePage> createState() => _MealManagementHomePageState();
+}
+
+class _MealManagementHomePageState extends State<MealManagementHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
